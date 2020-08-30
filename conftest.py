f163d51e8481f69e9bbcbcd6b7abd53dcaf2c3bb
LevonTonyan <levontonyan87@gmail.com>
2020-08-30 21:28:24 +0400

    emprovements added

--- conftest.py
+++ conftest.py
@@ -1,23 +1,36 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
 
 def pytest_addoption(parser):
-    parser.addoption('--browser_name', action='store', default=None,
-                     help="Choose browser: chrome or firefox")
+    parser.addoption('--page_language', action='store', default=None,
+                     help="Choose language: en, ru, fr or es")
+
+
+
+options = Options()
 
 
 @pytest.fixture(scope="function")
 def browser(request):
-    browser_name = request.config.getoption("browser_name")
+    page_language = request.config.getoption("page_language")
     browser = None
-    if browser_name == "chrome":
-        print("\nstart chrome browser for test..")
-        browser = webdriver.Chrome()
-    elif browser_name == "firefox":
-        print("\nstart firefox browser for test..")
-        browser = webdriver.Firefox()
+    if page_language == "es":
+        options.add_experimental_option('prefs', {'intl.accept_languages': page_language})
+        browser = webdriver.Chrome(options=options)
+    elif page_language == "ru":
+        options.add_experimental_option('prefs', {'intl.accept_languages': page_language})
+        browser = webdriver.Chrome(options=options)
+    elif page_language == "fr":
+        options.add_experimental_option('prefs', {'intl.accept_languages': page_language})
+        browser = webdriver.Chrome(options=options)
+    elif page_language == "en":
+        options.add_experimental_option('prefs', {'intl.accept_languages': page_language})
+        browser = webdriver.Chrome(options=options)
     else:
-        raise pytest.UsageError("--browser_name should be chrome or firefox")
+        raise pytest.UsageError("--page language should be en, fr, ru or es")
     yield browser
     print("\nquit browser..")
     browser.quit()
