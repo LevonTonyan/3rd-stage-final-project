f32e148f6dad0fab8ea6e841bd781ece13eb75b8
LevonTonyan <levontonyan87@gmail.com>
2020-08-30 21:43:07 +0400

    Final changes done

--- test_items.py
+++ test_items.py
@@ -1,9 +1,16 @@
-import time
+from selenium.common.exceptions import NoSuchElementException
+
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
 def test_guest_should_see_button_for_adding_item_to_cart(browser):
     browser.get(link)
-    time.sleep(30)
-    button = browser.find_element_by_css_selector("button[type='submit']")
-    assert button, 'Unable to locate the button'
+    css_selector = "button[type='submit']"
+    def check_exists_by_css(css_selector):
+        try:
+            browser.find_element_by_css_selector(css_selector)
+        except NoSuchElementException:
+            return False
+        return True
+
+    assert check_exists_by_css(css_selector) == True, 'Unable to locate the button'
